65ae295f8437c85e1bff053c19afafa7ff7d5525
beshoyraafat20 <beshoyraafat20@gmail.com>
2023-01-07 03:58:34 +0200

    version 1.0

--- snake_V1.0.py
+++ snake_V1.0.py
@@ -2,39 +2,9 @@
 import pygame
 import time
 import random
+from start_screen import *
+from snake_object import *
 
-snake_speed = 15
-
-# Window size
-window_x = 720
-window_y = 480
-
-# defining colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
-
-# Initialising pygame
-pygame.init()
-
-# Initialise game window
-pygame.display.set_caption('Snakes')
-game_window = pygame.display.set_mode((window_x, window_y))
-
-# FPS (frames per second) controller
-fps = pygame.time.Clock()
-
-# defining snake default position
-snake_position = [100, 50]
-
-# defining first 4 blocks of snake body
-snake_body = [[100, 50],
-			[90, 50],
-			[80, 50],
-			[70, 50]
-			]
 # fruit position
 fruit_position = [random.randrange(1, (window_x//10)) * 10,
 				random.randrange(1, (window_y//10)) * 10]
---
+++ snake_object.py
@@ -0,0 +1,13 @@
+snake_speed = 15
+
+# defining snake default position
+snake_position = [100, 50]
+
+# defining first 4 blocks of snake body
+snake_body = [[100, 50],
+			[90, 50],
+			[80, 50],
+			[70, 50]
+			]
+
+
