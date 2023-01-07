f42d2e9332853e20da1e77f5ac9b9e8274b181d2
beshoyraafat20 <beshoyraafat20@gmail.com>
2023-01-07 09:39:23 +0200

    v3

---
+++ config.py
@@ -0,0 +1,13 @@
+# screen size
+screen_width = 1080
+screen_height = 720
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Font sizes
+FONT_SMALL = 20
+FONT_MEDIUM = 36
+FONT_LARGE = 48
